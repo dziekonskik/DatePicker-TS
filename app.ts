94e6aa1c6548c00848f305e9dc83651b73fe6848
Seven <jaseveen@gmail.com>
2020-11-05 11:19:24 +0100

    reworked and finished

--- app.ts
+++ app.ts
@@ -22,56 +22,89 @@ const dateColumns: NodeListOf<HTMLDivElement> = document.querySelectorAll(
   '.datepicker__column'
 );
 
-let currentDay: number = 0;
-
-function reflectState(
-  element: HTMLDivElement,
-  calendarUnit: string[] | number[]
+function datePickerElement(
+  container: HtmlDivElement,
+  listOfDates: number[] | string[]
 ): void {
-  const [first, previous, current, next, last] = element.children;
-  const ifCurrentIsBegginingIamBeforetheEndOrTwoUnitsBehindCurrent: string = `${
-    currentDay === 0
-      ? calendarUnit[calendarUnit.length - 2]
-      : currentDay === 1
-      ? calendarUnit[calendarUnit.length - 1]
-      : calendarUnit[currentDay - 2]
-  }`;
-  const ifCurrentIsFirstIamLastOrOneBehindCurrent: string = `${
-    currentDay === 0
-      ? calendarUnit[calendarUnit.length - 1]
-      : calendarUnit[currentDay - 1]
-  }`;
-  const ifCurrentIsLastIAmFirstOrOneBeforeCurrent: string = `${
-    currentDay === calendarUnit.length - 1
-      ? calendarUnit[0]
-      : calendarUnit[currentDay + 1]
-  }`;
-  const ifCurrentIsLastIamSecondOrTwoUnitsAheadOfCurrent: string = `${
-    currentDay === calendarUnit.length - 1
-      ? calendarUnit[1]
-      : currentDay === calendarUnit.length - 2
-      ? calendarUnit[0]
-      : calendarUnit[currentDay + 2]
-  }`;
+  let first: HTMLSpanElement;
+  let prev: HTMLSpanElement;
+  let curr: HTMLSpanElement;
+  let next: HTMLSpanElement;
+  let last: HTMLSpanElement;
 
-  first.innerHTML = ifCurrentIsBegginingIamBeforetheEndOrTwoUnitsBehindCurrent;
-  previous.innerHTML = ifCurrentIsFirstIamLastOrOneBehindCurrent;
-  current.innerHTML = `${calendarUnit[currentDay]}`;
-  next.innerHTML = ifCurrentIsLastIAmFirstOrOneBeforeCurrent;
-  last.innerHTML = ifCurrentIsLastIamSecondOrTwoUnitsAheadOfCurrent;
+  let actionDB: number[] = [];
+
+  const classes = ['first', 'previous', 'current', 'next', 'last'];
+
+  listOfDates.forEach((date: string | number) => {
+    const span: HTMLSpanElement = document.createElement('span');
+    container.appendChild(span);
+    span.innerHTML = date;
+  });
+
+  curr = container.firstElementChild;
+  prev = curr.previousElementSibling || container.lastElementChild;
+  first = prev.previousElementSibling || container.lastElementChild;
+  next = curr.nextElementSibling || container.firstElementChild;
+  last = next.nextElementSibling || container.firstElementChild;
+
+  function addClasses() {
+    first.classList.add('first');
+    prev.classList.add('previous');
+    curr.classList.add('current');
+    next.classList.add('next');
+    last.classList.add('last');
+  }
+  addClasses();
+  function removeClasses() {
+    [first, prev, curr, next, last].map((item) =>
+      item.classList.remove(...classes)
+    );
+  }
+
+  function move(direction?: string) {
+    removeClasses();
+    if (direction === 'back') {
+      [first, prev, curr, next, last] = [
+        first.previousElementSibling || container.lastElementChild,
+        first,
+        prev,
+        curr,
+        next,
+      ];
+    } else {
+      [first, prev, curr, next, last] = [
+        prev,
+        curr,
+        next,
+        last,
+        last.nextElementSibling || container.firstElementChild,
+      ];
+    }
+    addClasses();
+  }
+  function calculateDirection(): number {
+    return actionDB.reduce((mousedown, mouseup) => mousedown - mouseup);
+  }
+
+  function handleMouseDown(event: MouseEvent): void {
+    actionDB.length = 0;
+    actionDB.unshift(event.clientY);
+  }
+
+  function handleMouseUp(event: MouseEvent) {
+    actionDB.unshift(event.clientY);
+    if (calculateDirection() > 0) {
+      move('back');
+    } else {
+      move();
+    }
+  }
+
+  container.addEventListener('mousedown', handleMouseDown);
+  container.addEventListener('mouseup', handleMouseUp);
 }
 
-console.log(dateColumns);
-
-const buttonInc = document.querySelector('.increment');
-const buttonDec = document.querySelector('.decrement');
-
-buttonInc?.addEventListener('click', (e) => {
-  currentDay = currentDay >= years.length - 1 ? 0 : currentDay + 1;
-  reflectState(dateColumns[2], years);
-});
-
-buttonDec?.addEventListener('click', (e) => {
-  currentDay = currentDay <= 0 ? years.length - 1 : currentDay - 1;
-  reflectState(dateColumns[2], years);
-});
+datePickerElement(dateColumns[0], days);
+datePickerElement(dateColumns[1], months);
+datePickerElement(dateColumns[2], years);
