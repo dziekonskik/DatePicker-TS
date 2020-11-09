389b064e377ebe7daa825f0792eaf5e69f42d1d4
Seven <jaseveen@gmail.com>
2020-11-09 14:23:08 +0100

    add more units

--- app.ts
+++ app.ts
@@ -26,18 +26,26 @@ function datePickerElement(
   container: HtmlDivElement,
   listOfDates: number[] | string[]
 ): void {
+  let pre: HTMLSpanElement;
+  let before: HTMLSpanElement;
   let first: HTMLSpanElement;
   let prev: HTMLSpanElement;
   let curr: HTMLSpanElement;
   let next: HTMLSpanElement;
   let last: HTMLSpanElement;
+  let after: HTMLSpanElement;
+  let pra: HTMLSpanElement;
 
   const availableClasses: string[] = [
+    'pre',
+    'before',
     'first',
     'previous',
     'current',
     'next',
     'last',
+    'after',
+    'pra',
   ];
 
   listOfDates.forEach((date: string | number) => {
@@ -49,19 +57,27 @@ function datePickerElement(
   curr = container.firstElementChild;
   prev = curr.previousElementSibling || container.lastElementChild;
   first = prev.previousElementSibling || container.lastElementChild;
+  before = first.previousElementSibling || container.lastElementChild;
+  pre = before.previousElementSibling || container.lastElementChild;
   next = curr.nextElementSibling || container.firstElementChild;
   last = next.nextElementSibling || container.firstElementChild;
+  after = last.nextElementSibling || container.firstElementChild;
+  pra = after.nextElementSibling || container.firstElementChild;
 
   function addClasses(): void {
+    pre.classList.add('pre');
+    before.classList.add('before');
     first.classList.add('first');
     prev.classList.add('previous');
     curr.classList.add('current');
     next.classList.add('next');
     last.classList.add('last');
+    after.classList.add('after');
+    pra.classList.add('pra');
   }
   addClasses();
   function removeClasses(): void {
-    [first, prev, curr, next, last].map((item) =>
+    [before, first, prev, curr, next, last, after].map((item) =>
       item.classList.remove(...availableClasses)
     );
   }
@@ -69,20 +85,28 @@ function datePickerElement(
   function move(direction?: string): void {
     removeClasses();
     if (direction === 'down') {
-      [first, prev, curr, next, last] = [
-        first.previousElementSibling || container.lastElementChild,
+      [pre, before, first, prev, curr, next, last, after, pra] = [
+        pre.previousElementSibling || container.lastElementChild,
+        pre,
+        before,
         first,
         prev,
         curr,
         next,
+        last,
+        after,
       ];
     } else {
-      [first, prev, curr, next, last] = [
+      [pre, before, first, prev, curr, next, last, after, pra] = [
+        before,
+        first,
         prev,
         curr,
         next,
         last,
-        last.nextElementSibling || container.firstElementChild,
+        after,
+        pra,
+        pra.nextElementSibling || container.firstElementChild,
       ];
     }
     addClasses();
